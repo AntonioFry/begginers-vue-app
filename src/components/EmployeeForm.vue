53cf9e799eb026d0edee1ca87ab244788b80517c
Antonio Fry <antoniofry2000@gmail.com>
2019-09-14 09:34:04 -0600

    render employee form to page

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form id="empployee-form">
+    <label>Employee name</label>
+    <input type="text" />
+    <label>Employee Email</label>
+    <input type="text" />
+    <button>Add Employee</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'employee-form',
+    data() {
+      return {
+        employee: {
+          name: '',
+          email: '',
+        },
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
